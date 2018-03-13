1ed6c057546b308a7826f02ea4cf39bf43427e1d
alunde <andrew.lunde@sap.com>
2018-03-13 23:18:41 +0000

    adding auth

--- python/server.py
+++ python/server.py
@@ -1,9 +1,20 @@
 import os
+import sapjwt
 from flask import Flask
 app = Flask(__name__)
 port = int(os.getenv("PORT", 9099))
 @app.route('/python/hello')
 def hello():
   return "Hello World"
+@app.route('/spython/auth')
+def auth_world():
+  output = 'Hello World : Authorized \n'
+  return output
+@app.route('/spython/valid')
+def auth_world():
+  output = 'Hello World : Validated \n'
+  jwtver = sapjwt.jwtValidation()
+  output += 'SAPJWT version : ' + jwtver.getLibraryVersion() + ' \n'
+  return output
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=port)
